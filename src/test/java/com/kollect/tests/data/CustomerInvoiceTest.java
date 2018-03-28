20abbbe38f25aa7c8627047b7f91166cf2ef1d8c
Joshua <jnwankwo@acm.org>
2018-03-28 15:22:19 +0800

    Initial commits and package name refectoring

--- src/test/java/com/kollect/tests/data/CustomerInvoiceTest.java
+++ src/test/java/com/kollect/tests/data/CustomerInvoiceTest.java
@@ -1,11 +1,16 @@
 package com.kollect.tests.data;
 
+import java.util.Map;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.kollect.etl.dataaccess.AbstractSqlSessionProvider;
 import com.kollect.tests.common.DaoProvider;
 import com.kollect.tests.common.ReadWriteServiceProvider;
+
+
 
 public class CustomerInvoiceTest {
   
@@ -19,9 +24,15 @@ public class CustomerInvoiceTest {
   
   @Test
   public void shouldNotContainDuplicateInvoice() {
+    Long expectedResult = 300000l;
+    @SuppressWarnings("unchecked")
+    Map<String, Long> map = (Map<String, Long>) service.executeQuery("getInvoiceCount", null).get(0);
+    Long actualResult = map.get("count");
+    Assert.assertEquals("Expected and actual are not same", expectedResult, actualResult);
     
-    System.out.println(service.executeQuery("getInvoiceCount", null));
     
+    //Assert.assertArrayEquals(expectedResult, actualResult);
+    //("Expected and actual are not same", expectedResult, actualResult);
   }
 
 }
