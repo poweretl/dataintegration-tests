0fd218fc5bd4d258220dbde212bc6534b1265e5d
Joshua <jnwankwo@acm.org>
2018-03-28 13:27:19 +0800

    Initial commit

---
+++ src/test/java/com/kollect/data/CustomerAddressTest.java
@@ -0,0 +1,5 @@
+package com.kollect.data;
+
+public class CustomerAddressTest {
+
+}
---
+++ src/test/java/com/kollect/data/InvoiceTest.java
@@ -0,0 +1,19 @@
+package com.kollect.data;
+
+//given-when-then
+
+public class InvoiceTest {
+  
+  
+  
+  public void run_once_per_test() {
+    
+  }
+  
+  void shouldNotHaveDuplicates(){}
+  
+  void shouldHaveExactQuatityOfInvoices() {}
+  
+  
+
+}
